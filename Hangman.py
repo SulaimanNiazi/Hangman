ea598633226f2da42ab4750e884920db3d6513cf
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2025-01-03 10:46:05 +0500

    Add get-guess and clear-screen functions

--- Hangman.py
+++ Hangman.py
@@ -1,4 +1,10 @@
+from os import system, name
 
+def clrscr():
+    if name=='nt':
+        system('cls')
+    else:
+        system('clear')
 def draw(errors):
     match errors:
         case 0:
@@ -105,8 +111,9 @@ You lost!
 """)
     return
 def randomWord():
-    import requests, random
-    x=requests.get('https://randomtextgenerator.com/')
+    from requests import get
+    from random import choice
+    x=get('https://randomtextgenerator.com/')
     lines=x.text.replace('<','>').split('>')
     words=[]
     found=False
@@ -114,7 +121,7 @@ def randomWord():
         if found:
             if line.__contains__('RandomTextGenerator.com') and len(words)>0:
                 while 1:
-                    word=random.choice(words).strip()
+                    word=choice(words).strip()
                     if len(word)>2:
                         return word.lower()
             if line is not 'br':
@@ -122,13 +129,42 @@ def randomWord():
         elif line.__contains__('div id="randomtext_box"'):
             found=True
     return ''
+def getGuess(forbidden=[]):
+    while 1:
+        guess=input('Guess a letter: ')
+        guess=guess.strip().lower()
+        if guess=='quit': return guess
+        elif len(guess) == 1 and guess not in forbidden:
+            if 123>ord(guess)>96:
+                return guess
+    #in case of incorrect input:
+        print('\nEnter 1 new alphabet only!\nEnter \'quit\' to give up\n')
 
 def main():
     mistakes=[]
     progress=[]
-    
+    word=randomWord()
+    letters=enumerate(word)
+    for each in letters:
+        progress.append('_')
+    while 1:
+        clrscr()
+        mistakeNum=len(mistakes)
+        draw(mistakeNum)
+        if mistakeNum>0:
+            print('Mistakes: ',''.join(map(lambda x:x+' ',mistakes)))
+            if mistakeNum>9:
+                print('\nThe word was: ',word)
+                return
+        print(''.join(map(lambda x:x+' ',progress)),'\n')
+        print(word)
+        guess=getGuess(mistakes+progress)
+        if guess=='quit':
+            print('The word was: ',word)
+            return
+        print('You guessed: ',guess)
+        return
 
 if __name__=="__main__":
-    import os
-    os.system('cls')
+    clrscr()
     main()
