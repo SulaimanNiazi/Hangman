0c7e8d20f4dc8981520ee34262d743ce6c7f1491
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2025-01-03 08:54:09 +0500

    Add random word generating function

--- Hangman.py
+++ Hangman.py
@@ -1,4 +1,23 @@
 
+def randomWord():
+    import requests, random
+    x=requests.get('https://randomtextgenerator.com/')
+    lines=x.text.replace('<','>').split('>')
+    words=[]
+    found=False
+    for line in lines:
+        if found:
+            if line.__contains__('RandomTextGenerator.com') and len(words)>0:
+                while 1:
+                    word=random.choice(words).strip()
+                    if len(word)>2:
+                        return word.lower()
+            if line is not 'br':
+                words.extend(line.replace('.','').split(' '))
+        elif line.__contains__('div id="randomtext_box"'):
+            found=True
+    return ''
+
 def main():
     mistakes=[]
     progress=[]
